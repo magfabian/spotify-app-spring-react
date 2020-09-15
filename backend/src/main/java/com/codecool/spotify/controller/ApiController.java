9a7ff803f2d54723ca72f8cb9943bbc9baddb873
nemethbence <DCdkm99dc>
2020-09-15 16:21:25 +0200

    Change ApiController

--- backend/src/main/java/com/codecool/spotify/controller/ApiController.java
+++ backend/src/main/java/com/codecool/spotify/controller/ApiController.java
@@ -1,10 +1,10 @@
 package com.codecool.spotify.controller;
 
-
 import com.codecool.spotify.model.Card;
 import com.codecool.spotify.service.DataProvider;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +19,10 @@ public class ApiController {
    @Autowired
    private DataProvider dataCreator;
 
+   @CrossOrigin
    @GetMapping("/new-releases")
-   public List<Card> getNewReleases() throws IOException, JSONException {
-      return dataCreator.getNewReleases();
+   public List<Card> handleNewReleases() throws IOException, JSONException {
+      return dataCreator.provideNewReleases();
    }
 
 }

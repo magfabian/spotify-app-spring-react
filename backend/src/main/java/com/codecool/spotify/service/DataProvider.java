455cdb02bc0b2ee0a238565ec0f37cfdd92695ea
nemethbence <DCdkm99dc>
2020-09-15 14:46:04 +0200

    Create DataProvider service

---
+++ backend/src/main/java/com/codecool/spotify/service/DataProvider.java
@@ -0,0 +1,35 @@
+package com.codecool.spotify.service;
+
+import com.codecool.spotify.model.Card;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DataProvider {
+
+
+   public static final String NEW_RELEASES_URL = "https://api.spotify.com/v1/browse/new-releases";
+   private static final String ACCESS_TOKEN_URL = "?access_token=";
+
+   @Autowired
+   private RemoteURLReader remoteURLReader;
+
+   @Autowired
+   private TokenProvider tokenProvider;
+
+   public List<Card> getNewReleases() throws IOException, JSONException {
+      List<Card> data = new ArrayList();
+      String token = tokenProvider.getAccessToken();
+      String url = NEW_RELEASES_URL + ACCESS_TOKEN_URL + token;
+      JSONObject json = remoteURLReader.readFromUrl(url);
+      return data;
+   }
+
+
+}

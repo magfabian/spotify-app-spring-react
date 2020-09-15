94237bf8bcce8718012f11bd68cc0079831f91bf
nemethbence <DCdkm99dc>
2020-09-15 14:44:56 +0200

    Create ApiController

---
+++ backend/src/main/java/com/codecool/spotify/controller/ApiController.java
@@ -0,0 +1,27 @@
+package com.codecool.spotify.controller;
+
+
+import com.codecool.spotify.model.Card;
+import com.codecool.spotify.service.DataProvider;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+   @Autowired
+   private DataProvider dataCreator;
+
+   @GetMapping("/new-releases")
+   public List<Card> getNewReleases() throws IOException, JSONException {
+      return dataCreator.getNewReleases();
+   }
+
+}

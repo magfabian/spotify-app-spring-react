f6e1e80fe335c44bd972bf6e6e277015f975497e
nemethbence <DCdkm99dc>
2020-09-15 14:46:21 +0200

    Create RemoteURLReader

---
+++ backend/src/main/java/com/codecool/spotify/service/RemoteURLReader.java
@@ -0,0 +1,28 @@
+package com.codecool.spotify.service;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@Component
+public class RemoteURLReader {
+
+   public JSONObject readFromUrl(String endpoint) throws IOException, JSONException {
+      URL url = new URL(endpoint);
+      URLConnection conn = url.openConnection();
+      String lines;
+      try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+         lines = reader.lines().collect(Collectors.joining("\n"));
+      }
+      JSONObject json = new JSONObject(lines);
+      return json;
+   }
+}

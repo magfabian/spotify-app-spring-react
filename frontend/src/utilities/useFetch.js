19de4d9b6f265f78aa4bff9ad04f20d744508073
nemethbence <DCdkm99dc>
2020-09-16 11:42:02 +0200

    Change useFetch

--- frontend/src/utilities/useFetch.js
+++ frontend/src/utilities/useFetch.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import token from "./token";
 
 const useFetch = (url, route) => {
     const [status, setStatus] = useState("");
@@ -18,29 +17,12 @@ const useFetch = (url, route) => {
             .get(url)
             .then(response => {
                 setStatus("loaded");
-                setData(getObjectRoute(response));
+                setData(response.data);
             })
             .catch(error => {
                 setStatus("error");
                 setError(error.response);
             });
-    };
-
-    const getObjectRoute = response => {
-        switch (route) {
-            case "release":
-                return response.data;
-            case "artist":
-                return response.data.artists.items;
-            case "track":
-                return response.data.tracks.items;
-            case "playlist":
-                return response.data.playlists.items;
-            case "album":
-                return response.data.albums.items;
-            default:
-                return "error";
-        }
     };
 
     return [status, error, fetchedData];

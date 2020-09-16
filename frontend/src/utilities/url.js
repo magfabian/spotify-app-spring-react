bea29f5dcae936585c5ed6bbd6585017c1e2c9be
nemethbence <DCdkm99dc>
2020-09-16 12:02:59 +0200

    Change url

--- frontend/src/utilities/url.js
+++ frontend/src/utilities/url.js
@@ -1,8 +1,8 @@
 const url = {
     new_releases: "/api/new-releases",
-    album_search: "/api/search/album/",
+    album_search: "/api/search/albums/",
     playlist_search: "/api/search/playlists/",
-    artist_search: "/api/search/artist/",
+    artist_search: "/api/search/albums/",
     track_search: "/api/search/tracks/",
     noImg:
         "https://748073e22e8db794416a-cc51ef6b37841580002827d4d94d19b6.ssl.cf3.rackcdn.com/not-found.png"

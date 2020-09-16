c0b7a3800f2590509c052cc99d24909cde9cdf37
magfabian <mag.fabian87@gmail.com>
2020-09-16 10:30:53 +0200

    Add new url to ApiController to handle searched tracks

--- backend/src/main/java/com/codecool/spotify/controller/ApiController.java
+++ backend/src/main/java/com/codecool/spotify/controller/ApiController.java
@@ -4,10 +4,7 @@ import com.codecool.spotify.model.Card;
 import com.codecool.spotify.service.DataProvider;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,6 +20,12 @@ public class ApiController {
    @GetMapping("/new-releases")
    public List<Card> handleNewReleases() throws IOException, JSONException {
       return dataCreator.provideNewReleases();
+   }
+
+   @CrossOrigin
+   @GetMapping("/tracks/{searchString}")
+   public List<Card> handleTrackSearch(@PathVariable String searchString) throws IOException, JSONException {
+      return dataCreator.provideTracks(searchString);
    }
 
 }

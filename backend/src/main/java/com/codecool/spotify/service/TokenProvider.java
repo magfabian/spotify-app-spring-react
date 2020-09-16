707faf79ac4a2a55b2345075ec511bf82baa8e37
nemethbence <DCdkm99dc>
2020-09-16 16:17:04 +0200

    Change TokenProvider

--- backend/src/main/java/com/codecool/spotify/service/TokenProvider.java
+++ backend/src/main/java/com/codecool/spotify/service/TokenProvider.java
@@ -43,7 +43,6 @@ public class TokenProvider {
    private String getToken() throws IOException, JSONException {
       JSONObject json = remoteURLReader.readFromUrl(TOKEN_URL,"");
       String token = json.get("token").toString();
-      System.out.println(token);
       return token;
    }
 

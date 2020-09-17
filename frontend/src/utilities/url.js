2f1e70e9b3e2910edc32412a80b354d9c2cc272f
nembence <nemeth.bence@icloud.com>
2020-09-17 12:51:40 +0200

    Change url

--- frontend/src/utilities/url.js
+++ frontend/src/utilities/url.js
@@ -10,6 +10,7 @@ const url = {
     favorite_playlist: "/favorite/playlist",
     favorite_all: "/favorite/get-all",
     playlist_new: "/playlist/new/",
+    playlist_get_all: "/playlist/get-all",
 };
 
 export default url;

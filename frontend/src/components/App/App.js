ad66068d010831d89410e42270197f7dcf6ddf95
nembence <nemeth.bence@icloud.com>
2020-09-17 09:42:20 +0200

    Change App component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -6,38 +6,35 @@ import Navbar from "../Navbar/Navbar";
 import Favorite from "../Favorite/Favorite";
 import Category from "../Category/Category";
 import "./App.css";
-import { FavouriteProvider } from "../../context/FavouriteContext";
 
 function App() {
     return (
-        <FavouriteProvider>
-            <Router>
-                <Header />
-                <div className='wrapper'>
-                    <Navbar />
-                    <Switch>
-                        <Route exact path='/'>
-                            <Home />
-                        </Route>
-                        <Route path='/favorite'>
-                            <Favorite />
-                        </Route>
-                        <Route path='/artist'>
-                            <Category categoryType={"artist"} />
-                        </Route>
-                        <Route path='/album'>
-                            <Category categoryType={"album"} />
-                        </Route>
-                        <Route path='/track'>
-                            <Category categoryType={"track"} />
-                        </Route>
-                        <Route path='/playlist'>
-                            <Category categoryType={"playlist"} />
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        </FavouriteProvider>
+        <Router>
+            <Header />
+            <div className="wrapper">
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/favorite">
+                        <Favorite />
+                    </Route>
+                    <Route path="/artist">
+                        <Category categoryType={"artist"} />
+                    </Route>
+                    <Route path="/album">
+                        <Category categoryType={"album"} />
+                    </Route>
+                    <Route path="/track">
+                        <Category categoryType={"track"} />
+                    </Route>
+                    <Route path="/playlist">
+                        <Category categoryType={"playlist"} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 

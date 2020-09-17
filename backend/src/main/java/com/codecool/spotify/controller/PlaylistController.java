fd4c25f6131e0a76f24cc7e177d3d106d44dd007
magfabian <mag.fabian87@gmail.com>
2020-09-17 10:13:54 +0200

    Add PlaylistController to handle own playlists

---
+++ backend/src/main/java/com/codecool/spotify/controller/PlaylistController.java
@@ -0,0 +1,33 @@
+package com.codecool.spotify.controller;
+
+import com.codecool.spotify.model.Card;
+import com.codecool.spotify.service.PlaylistProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/playlist")
+public class PlaylistController {
+
+    @Autowired
+    PlaylistProvider playlistProvider;
+
+    @CrossOrigin
+    @GetMapping("/get-all")
+    public Map<String, List<Card>> handleAllPlaylist() {
+        return playlistProvider.getAllPlaylists();
+    }
+
+    @PostMapping("/new/{playlist}")
+    public void handleNewPlaylist(@PathVariable String playlist) {
+        playlistProvider.addNewPlaylist(playlist);
+    }
+
+    @PostMapping("/track/{playlist}/")
+    public void handleNewTrack(@PathVariable String playlist, @RequestBody Card card) {
+        playlistProvider.addNewTrack(playlist, card);
+    }
+}

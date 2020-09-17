b53036b64ab1304c77ae1f49189de126789effd4
magfabian <mag.fabian87@gmail.com>
2020-09-17 10:29:38 +0200

    Create hashmap to return lengths of saved playlists

--- backend/src/main/java/com/codecool/spotify/service/PlaylistProvider.java
+++ backend/src/main/java/com/codecool/spotify/service/PlaylistProvider.java
@@ -11,16 +11,24 @@ import java.util.Map;
 public class PlaylistProvider {
 
     private Map<String, List<Card>> playlists = new HashMap<>();
+    private Map<String, Integer> playlistsLength = new HashMap<>();
 
     public Map<String, List<Card>> getAllPlaylists() {
         return this.playlists;
     }
 
     public void addNewPlaylist(String title) {
+        playlistsLength.put(title, 0);
         playlists.put(title, new ArrayList<>());
     }
 
     public void addNewTrack(String title, Card card) {
+        playlistsLength.remove(title, playlistsLength.get(title));
+        playlistsLength.put(title, playlists.get(title).size());
         playlists.get(title).add(card);
+    }
+
+    public Map<String, Integer> getPlaylistsLength() {
+        return playlistsLength;
     }
 }

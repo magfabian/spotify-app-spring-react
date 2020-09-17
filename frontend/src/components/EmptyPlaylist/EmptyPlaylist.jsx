75e581b92584b2fded2f6b2155a6340f2fa03acf
nembence <nemeth.bence@icloud.com>
2020-09-17 19:30:01 +0200

    Change EmptyPlaylist

--- frontend/src/components/EmptyPlaylist/EmptyPlaylist.jsx
+++ frontend/src/components/EmptyPlaylist/EmptyPlaylist.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Header, Divider } from "semantic-ui-react";
 
 const EmptyPlaylist = () => {
+    const dividerStlye = {
+        maxWidth: "940px",
+    };
+
+    const headerStyle = {
+        fontSize: "30px",
+        textDecoration: "underline",
+        textDecorationColor: "rgb(85,182,96)",
+        fontWeight: "bolder",
+        marginTop: "20px",
+    };
+
     return (
         <aside>
-            <p>Oops.</p>
-            <p>The Playlist is empty</p>
-            <a href="/track">Fill playlist with your favorite tracks!</a>
+            <Divider style={dividerStlye} horizontal>
+                TRACKS
+            </Divider>
+            <Header as="h2">Your playlist is empty.</Header>
+            <a href="/track">
+                <Header as="h3" style={headerStyle}>
+                    Fill playlist with your favorite tracks!
+                </Header>
+            </a>
         </aside>
     );
 };

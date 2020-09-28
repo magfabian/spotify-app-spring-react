b1a4ce864ca55a1c3046e55683524846ea002cd5
magfabian <mag.fabian87@gmail.com>
2020-09-28 14:20:33 +0200

    Add new model classes

---
+++ backend/src/main/java/com/codecool/spotify/model/Artist.java
@@ -0,0 +1,14 @@
+package com.codecool.spotify.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Artist extends Card {
+}

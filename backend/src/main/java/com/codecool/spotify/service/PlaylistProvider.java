4866915fe4c8a0d80a116c2fb5f453108a5bd51b
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2020-09-29 14:23:22 +0200

    Change services

--- backend/src/main/java/com/codecool/spotify/service/PlaylistProvider.java
+++ backend/src/main/java/com/codecool/spotify/service/PlaylistProvider.java
@@ -1,24 +1,40 @@
 package com.codecool.spotify.service;
 
+import com.codecool.spotify.model.Track;
 import com.codecool.spotify.model.UserPlaylist;
+import com.codecool.spotify.repository.UserPlaylistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
+
+import java.util.HashSet;
 import java.util.List;
 
 @Service
 public class PlaylistProvider {
 
-    private List<UserPlaylist> userPlaylists = new ArrayList<>();
+    @Autowired
+    private UserPlaylistRepository userPlaylistRepository;
 
     public List<UserPlaylist> getAllPlaylists() {
-        return this.userPlaylists;
+        return userPlaylistRepository.findAll();
     }
 
     public void addNewPlaylist(String title) {
-       userPlaylists.add(new UserPlaylist(title));
+       UserPlaylist userPlaylist = UserPlaylist.builder()
+               .title(title)
+               .total(0)
+               .tracks(new HashSet<>())
+               .build();
+       userPlaylistRepository.save(userPlaylist);
     }
 
     public UserPlaylist getSpecificPlaylist(String title) {
-        return userPlaylists.stream().filter(userPlaylist -> userPlaylist.getTitle().equals(title)).findFirst().get();
+        return userPlaylistRepository.findUserPlaylistByTitle(title);
+    }
+
+    public void addNewTrackToPlaylist(String title, Track track) {
+        UserPlaylist userPlaylist = userPlaylistRepository.findUserPlaylistByTitle(title);
+        userPlaylist.getTracks().add(track);
+        userPlaylistRepository.save(userPlaylist);
     }
 }

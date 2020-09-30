605b2df8c6bc070bd101ec2a8a0940fccaa16649
magfabian <mag.fabian87@gmail.com>
2020-09-30 14:13:22 +0200

    Refactor

--- backend/src/test/java/com/codecool/spotify/repository/FavoriteAlbumRepositoryTest.java
+++ backend/src/test/java/com/codecool/spotify/repository/FavoriteAlbumRepositoryTest.java
@@ -8,9 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 

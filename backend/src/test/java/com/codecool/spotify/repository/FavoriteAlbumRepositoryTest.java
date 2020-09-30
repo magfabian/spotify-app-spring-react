5115537025187dba2226ef3faa090f11d067becc
magfabian <mag.fabian87@gmail.com>
2020-09-30 13:57:49 +0200

    Test saving the same albums

--- backend/src/test/java/com/codecool/spotify/repository/FavoriteAlbumRepositoryTest.java
+++ backend/src/test/java/com/codecool/spotify/repository/FavoriteAlbumRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -37,5 +38,29 @@ class FavoriteAlbumRepositoryTest {
         List<Album> albumList = favoriteAlbumRepository.findAll();
 
         assertThat(albumList).hasSize(1);
+    }
+
+    @Test
+    public void saveSameAlbumTwice() {
+        Album album = Album.builder()
+            .spotifyId("4Bp7LKA5Afo1PRoXuQe8qZ")
+            .imageUrl("https://i.scdn.co/image/ab67616d0000b27370f2ab5608885749f7210b5f")
+            .onClickUrl("https://open.spotify.com/album/4Bp7LKA5Afo1PRoXuQe8qZ")
+            .header("FRANCHISE (feat. Young Thug & M.I.A.)")
+            .footer("Travis Scott")
+            .footerUrl("https://open.spotify.com/artist/0Y5tJX1MQlPlqiwlOH1tJY")
+            .favorite(true)
+            .build();
+
+        favoriteAlbumRepository.save(album);
+
+        Album album2 = Album.builder()
+            .spotifyId("4Bp7LKA5Afo1PRoXuQe8qZ")
+            .favorite(true)
+            .build();
+
+        assertThrows(DataIntegrityViolationException.class,  () ->
+            favoriteAlbumRepository.saveAndFlush(album2)
+        );
     }
 }

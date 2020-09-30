52c74bea6c25572d81b5f438b3ad0969380add61
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2020-09-30 10:00:32 +0200

    Create UserPlaylistTrack

---
+++ backend/src/main/java/com/codecool/spotify/model/UserPlaylistTrack.java
@@ -0,0 +1,42 @@
+package com.codecool.spotify.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserPlaylistTrack {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String spotifyId;
+
+    @Column
+    private String imageUrl;
+
+    @Column
+    private String onClickUrl;
+
+    @Column
+    private String header;
+
+    @Column
+    private String footer;
+
+    @Column
+    private String footerUrl;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    private UserPlaylist userPlaylist;
+}

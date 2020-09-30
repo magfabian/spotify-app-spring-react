0178e9f5f20a540e430a6fdd47732488eb2d998e
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2020-09-30 09:59:53 +0200

    Change UserPlaylist annotations

--- backend/src/main/java/com/codecool/spotify/model/UserPlaylist.java
+++ backend/src/main/java/com/codecool/spotify/model/UserPlaylist.java
@@ -1,8 +1,10 @@
 package com.codecool.spotify.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -19,11 +21,11 @@ public class UserPlaylist {
     @Column(nullable = false)
     private String title;
 
-    @Transient
     private int total;
 
-    @ManyToMany(cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "userPlaylist", cascade = CascadeType.ALL)
     @Singular
-    private Set<Track> tracks;
+    @EqualsAndHashCode.Exclude
+    private Set<UserPlaylistTrack> userPlaylistTracks = new HashSet<>();
 
 }

bb560baceda5f3ca0e24a01793feca6a03748a08
nembence <nemeth.bence@icloud.com>
2020-10-01 16:04:07 +0200

    Change repository

--- backend/src/main/java/com/codecool/spotify/repository/FavoriteTrackRepository.java
+++ backend/src/main/java/com/codecool/spotify/repository/FavoriteTrackRepository.java
@@ -4,4 +4,6 @@ import com.codecool.spotify.model.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteTrackRepository extends JpaRepository<Track, Long> {
+
+    void deleteTrackBySpotifyId(String spotifyId);
 }

df17de61e261aaaf91c8f962aabb15a9fc048baf
nembence <nemeth.bence@icloud.com>
2020-10-01 16:04:39 +0200

    Change FavoriteStar

--- frontend/src/components/FavoriteStar/FavoriteStar.jsx
+++ frontend/src/components/FavoriteStar/FavoriteStar.jsx
@@ -50,11 +50,48 @@ const FavoriteStar = ({
         }
     };
 
+    const handleYellowClick = (event) => {
+        event.target.className = "star large icon";
+        const card = {
+            spotifyId: spotifyId,
+            imageUrl: imageUrl,
+            onClickUrl: onClickUrl,
+            header: header,
+            footer: footer,
+            footerUrl: footerUrl,
+            favorite: favorite,
+        };
+        handleDelete(card);
+    };
+
+    const handleDelete = (card) => {
+        switch (category) {
+            case "track":
+                axios.post(url.favorite_delete_track, card);
+                break;
+            case "album":
+                axios.post(url.favorite_delete_album, card);
+                break;
+            case "playlist":
+                axios.post(url.favorite_delete_playlist, card);
+                break;
+            case "artist":
+                axios.post(url.favorite_delete_artist, card);
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
         <Card.Content extra>
             <span style={starStyle}>
                 {favorite === true ? (
-                    <Icon name="yellow star" size="large" />
+                    <Icon
+                        name="yellow star"
+                        size="large"
+                        onClick={handleYellowClick}
+                    />
                 ) : (
                     <Icon name="star" size="large" onClick={handleClick} />
                 )}

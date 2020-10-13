b33df194e288a5c2f0fbb27d6f833da3fc5740d4
magfabian <mag.fabian87@gmail.com>
2020-10-13 14:12:38 +0200

    Add security config to project

---
+++ backend/src/main/java/com/codecool/spotify/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.codecool.spotify.security;
+
+import com.codecool.spotify.service.SpotiUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private SpotiUserDetailService spotiUserDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(spotiUserDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeRequests()
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/signup").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .addFilterBefore(JwtFilter.builder()
+                    .jwtService(jwtService)
+                    .build(),
+                    UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}

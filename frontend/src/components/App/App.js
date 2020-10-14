078883b1b8f90a4b8a1fe4573884017feb499e42
nembence <nemeth.bence@icloud.com>
2020-10-14 10:52:19 +0200

    Add new routes to App component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -8,43 +8,51 @@ import Category from "../Category/Category";
 import "./App.css";
 import Playlists from "../Playlists/Playlists";
 import Playlist from "../Playlist/Playlist";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <div className="wrapper">
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/favorite">
-            <Favorite />
-          </Route>
-          <Route exact path="/playlists">
-            <Playlists />
-          </Route>
-          <Route path="/artist">
-            <Category categoryType={"artist"} />
-          </Route>
-          <Route path="/album">
-            <Category categoryType={"album"} />
-          </Route>
-          <Route path="/track">
-            <Category categoryType={"track"} />
-          </Route>
-          <Route path="/playlist">
-            <Category categoryType={"playlist"} />
-          </Route>
-          <Route
-            path="/playlists/:title"
-            render={(props) => <Playlist {...props} />}
-          ></Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <div className="wrapper">
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/favorite">
+                        <Favorite />
+                    </Route>
+                    <Route exact path="/playlists">
+                        <Playlists />
+                    </Route>
+                    <Route path="/artist">
+                        <Category categoryType={"artist"} />
+                    </Route>
+                    <Route path="/album">
+                        <Category categoryType={"album"} />
+                    </Route>
+                    <Route path="/track">
+                        <Category categoryType={"track"} />
+                    </Route>
+                    <Route path="/playlist">
+                        <Category categoryType={"playlist"} />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <Signup />
+                    </Route>
+                    <Route
+                        path="/playlists/:title"
+                        render={(props) => <Playlist {...props} />}
+                    ></Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;

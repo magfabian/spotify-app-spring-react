ba19a44bdf817e2ef2170b2c0bb66c6062da3ade
nembence <nemeth.bence@icloud.com>
2020-10-14 17:02:40 +0200

    Change UserController

--- backend/src/main/java/com/codecool/spotify/controller/UserController.java
+++ backend/src/main/java/com/codecool/spotify/controller/UserController.java
@@ -3,10 +3,9 @@ package com.codecool.spotify.controller;
 import com.codecool.spotify.model.user.SpotiUser;
 import com.codecool.spotify.service.SpotiUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +17,11 @@ public class UserController {
     @PostMapping("/signup")
     public void handleNewUser(@RequestBody SpotiUser spotiUser) {
         spotiUserService.saveNewUser(spotiUser);
+    }
+
+    @GetMapping("/me")
+    public Object currentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getPrincipal();
     }
 }

05265d624ae4589b654a771934e68909e099229e
magfabian <mag.fabian87@gmail.com>
2020-10-14 16:06:13 +0200

    Change Imports

--- backend/src/main/java/com/codecool/spotify/controller/AuthController.java
+++ backend/src/main/java/com/codecool/spotify/controller/AuthController.java
@@ -2,9 +2,8 @@ package com.codecool.spotify.controller;
 
 import com.codecool.spotify.model.user.SpotiUserCredentials;
 import com.codecool.spotify.security.JwtService;
-import com.codecool.spotify.service.SpotiUserDetailService;
-import com.codecool.spotify.service.SpotiUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,12 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.Cookie;
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import java.util.HashMap;

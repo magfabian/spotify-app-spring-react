5c8ceb7cd7ea996a8c8413865ef25f08e32b96f0
magfabian <mag.fabian87@gmail.com>
2020-10-15 11:47:04 +0200

    Delete favorite Object from user by spotiId

--- backend/src/main/java/com/codecool/spotify/repository/favorite/FavoriteAlbumRepository.java
+++ backend/src/main/java/com/codecool/spotify/repository/favorite/FavoriteAlbumRepository.java
@@ -1,9 +1,17 @@
 package com.codecool.spotify.repository.favorite;
 
 import com.codecool.spotify.model.favorite.Album;
+import com.codecool.spotify.model.user.SpotiUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface FavoriteAlbumRepository extends JpaRepository<Album, Long> {
 
-    void deleteAlbumBySpotifyId(String spotifyId);
+    @Query("DELETE FROM Album a WHERE a.spotifyId = :spotifyId AND a.spotiUser = :spotiUser")
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    void deleteAlbumBySpotifyId(SpotiUser spotiUser, String spotifyId);
 }
--- backend/src/main/java/com/codecool/spotify/repository/favorite/FavoriteTrackRepository.java
+++ backend/src/main/java/com/codecool/spotify/repository/favorite/FavoriteTrackRepository.java
@@ -1,9 +1,17 @@
 package com.codecool.spotify.repository.favorite;
 
 import com.codecool.spotify.model.favorite.Track;
+import com.codecool.spotify.model.user.SpotiUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface FavoriteTrackRepository extends JpaRepository<Track, Long> {
 
-    void deleteTrackBySpotifyId(String spotifyId);
+    @Query("DELETE FROM Track t WHERE t.spotifyId = :spotifyId AND t.spotiUser = :spotiUser")
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    void deleteTrackBySpotifyId(SpotiUser spotiUser, String spotifyId);
 }

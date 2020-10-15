f0c490c0c9cbb227bfc3ed073ba3ed193ff84fd2
nembence <nemeth.bence@icloud.com>
2020-10-15 16:15:35 +0200

    Change UserController

--- backend/src/main/java/com/codecool/spotify/controller/UserController.java
+++ backend/src/main/java/com/codecool/spotify/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.codecool.spotify.controller;
 
-import com.codecool.spotify.model.user.SpotiUser;import com.codecool.spotify.service.SpotiUserService;
+import com.codecool.spotify.model.user.SpotiUser;
+import com.codecool.spotify.repository.user.SpotiUserRepository;
+import com.codecool.spotify.service.SpotiUserService;
+import com.codecool.spotify.utility.PrincipalFinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +17,9 @@ public class UserController {
 
     @Autowired
     private SpotiUserService spotiUserService;
+
+    @Autowired
+    private SpotiUserRepository spotiUserRepository;
 
     @PostMapping("/signup")
     public void handleNewUser(@RequestBody SpotiUser spotiUser) {
@@ -31,5 +38,14 @@ public class UserController {
     public Object currentUser(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getPrincipal();
+    }
+
+    @GetMapping("/profile")
+    public SpotiUser user(){
+        String email = PrincipalFinder.getCurrentlyLoggedInUserEmail();
+        SpotiUser user = spotiUserRepository.findSpotiUserByEmailAddress(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+        user.setPassword("");
+        return user;
     }
 }
